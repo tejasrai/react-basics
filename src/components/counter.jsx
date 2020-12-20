92663760e3770b52a3447c912f16fbf3a100228d
tejasrai <tejas.rai.phe17@iitbhu.ac.in>
2020-12-20 19:59:11 +0530

    styling buttons using bootstrap css classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 class Counter extends Component {
+    state={
+        count:0
+    };
     render() { 
-        return <h1>Hello World.</h1>;
+        return (<React.Fragment><span className="badge badge-primary m-2">{this.formatCount()}</span><button className="btn btn-secondary btn-sm">Increment</button></React.Fragment>);
+    }
+    formatCount(){
+        const {count}=this.state;
+        return count===0?'Zero':count;
     }
 }
  
