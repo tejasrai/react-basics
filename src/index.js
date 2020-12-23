f3f26ece1e7e544ca6a4c8e448c0a92c8ee22cec
tejasrai <tejas.rai.phe17@iitbhu.ac.in>
2020-12-23 11:26:18 +0530

    adding navbar and multiple components in sync

--- src/index.js
+++ src/index.js
@@ -4,13 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counters from './components/counters';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Counters />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
